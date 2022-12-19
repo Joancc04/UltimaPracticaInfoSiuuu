966553571757cca9120b80aba04d5715df5f204f
Joancc04 <joancolillas2004@gmail.com>
2022-12-19 16:03:26 +0100

    versió final

--- my_cp.c
+++ my_cp.c
@@ -7,17 +7,20 @@
 int main(){
     char buffer[N];
     scanf("%s", buffer);
-    int res = open(buffer, O_RDONLY), desc;
-    int r;
+    int res = open(buffer, O_RDONLY);
+    int r, desc;
     if (res == -1){
         printf("ERROR: L'arxiu no existeix");
     } else {
-        if (desc=open("copia.txt", O_CREAT|O_WRONLY|O_TRUNC,0777 ) == -1){
+        desc=open("copia.txt", O_CREAT|O_WRONLY|O_TRUNC,S_IRWXU);
+        if (desc == -1){
             printf("Error al crear arxiu");
         }
         else{
-            while((r = read(res, buffer, N-1))!=0) write(desc, buffer, N-1);
+            while((r = read(res, buffer, N-1))!=0)
+                {write(desc, buffer, r);}
         }
     }
-    close(desc);   
+    close(desc);
+    close(res);
 }
